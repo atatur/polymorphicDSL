a19da5fea50d2134a2275b4ca3e8ff367fa5423f
Nelson <62027792+incident-recipient@users.noreply.github.com>
2026-05-08 06:09:28 -0700

    Update InterpreterDuplicatePhrasesException.java

--- src/main/java/com/pdsl/exceptions/InterpreterDuplicatePhrasesException.java
+++ src/main/java/com/pdsl/exceptions/InterpreterDuplicatePhrasesException.java
@@ -4,8 +4,7 @@ package com.pdsl.exceptions;
  * Exception thrown when an interpreter encounters duplicate phrases in a case of NO_DUPLICATES_PHRASES interpreter constraint.
  *
  * <p>In the context of PDSL, this typically occurs when the same phrase is
- * processed multiple times by the same interpreter in a way that is not
- * allowed by the current configuration.</p>
+ * processed by the more than one interpreter when the NO_DUPLICATE_PHRASES contraint is provided.</p>
  */
 public class InterpreterDuplicatePhrasesException extends RuntimeException {
 
